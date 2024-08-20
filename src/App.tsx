249cd739f71cf28628a14cb45c59a66e7a42f4bf
Leela_Krishna <leela_krishna@dell.com>
2024-08-20 19:23:58 +0530

    Final code

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import Todo from "./components/Todo";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>Hey</div>
+      <div className="bg-stone-900 grid py-4 min-h-screen">
+        <Todo />
+      </div>
     </>
   );
 }
